d1c50bcd9d8393e5375848a1cd832b2f5d3fda77
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-02-06 13:50:41 +0100

    Set version to 0.1.0

--- src/AppConstants.cs
+++ src/AppConstants.cs
@@ -6,6 +6,6 @@ public static class AppConstants
 {
     public const string Title = "Rayman 1 Tool";
     public const string Description = "Tools for Rayman 1";
-    public static Version Version => new Version(0, 0, 0, 0); // Not currently released
+    public static Version Version => new Version(0, 1, 0, 0); // Not currently released
     public const string GitHubURL = "https://github.com/RayCarrot/RayCarrot.BizHawk.R1Tool";
 }
